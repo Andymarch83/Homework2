cb4bf2db9b5aa7473d874026ec5934ab8352115d
Andrey <5657031@gmail.com>
2023-02-02 03:59:20 +0300

    fixed

---
+++ h1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+//456 -> 5
+//782 -> 8
+//918 -> 1 
+int num2(int number)
+{
+    int firstCouple = number / 10;
+    int firstDigit = number / 100;
+    int result =(firstCouple - (firstDigit*10));
+    return result;
+}
+
+Console.WriteLine("введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ($"{number} -> {num2(number)}");
---
+++ h2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+ 645 -> 5
+
+ 78 -> третьей цифры нет
+
+ 32679 -> 6 */
+Console.WriteLine("Введите число:"); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+string numberStr = Math.Abs(number).ToString(); 
+
+if(numberStr.Length > 2) 
+{ 
+ Console.WriteLine($"{number} -> {numberStr[2]}"); 
+} 
+else 
+{ 
+ Console.WriteLine($"{number} -> третьей цифры нет"); 
+}
